1b6a8e83332d7083b149a15406a53bac4b35c3e6
Andrea Scarpello <aduptive@gmail.com>
2024-07-10 02:06:51 -0300

    chore: update & fix config

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,11 +6,12 @@ local M = {}
 
 M.ui = {
 	theme = "chadracula-evondev",
-
 	-- hl_override = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
 	-- },
 }
+
+M.plugins = "custom.plugins"
 
 return M
